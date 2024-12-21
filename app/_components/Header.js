fc4e90ae5df37c4f61280b067348a4706569920d
sankarkrishnan07 <krishna07off@gmail.com>
2024-12-21 15:42:24 +0530

    initial setup

---
+++ app/_components/Header.js
@@ -0,0 +1,13 @@
+import Logo from "./Logo";
+import Navigation from "./Navigation";
+
+function Header() {
+    return <header className="border-b border-primary-100 px-8 py-5">
+        <div className="flex justify-between items-center max-w-7xl mx-auto">
+            <Logo />
+            <Navigation />
+        </div>
+    </header>
+}
+
+export default Header;
---
+++ app/loading.js
@@ -0,0 +1,5 @@
+function loading() {
+    return <p>Loading Data...</p>
+}
+
+export default loading;
