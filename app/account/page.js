eefacf6cdd13fd3d43405f9a98e2e3063c6fe27d
sankarkrishnan07 <krishna07off@gmail.com>
2024-12-29 00:50:26 +0530

    Implemented custom signin, signout and authorization using next js middleware

--- app/account/page.js
+++ app/account/page.js
@@ -1,11 +1,18 @@
+import { auth } from "../_lib/auth";
+
 export const metadata = {
-    title: "Guest area",
-  };
-  
-  export default function Page() {
-    return (
-      <h2 className="font-semibold text-2xl text-accent-400 mb-7">
-        Welcome, Sankar
-      </h2>
-    );
-  }
+  title: "Guest area",
+};
+
+export default async function Page() {
+  const session = await auth();
+  const firstName = session.user.name.split(" ").at(0);
+
+  console.log(session)
+
+  return (
+    <h2 className="font-semibold text-2xl text-accent-400 mb-7">
+      Welcome, {firstName}
+    </h2>
+  );
+}
