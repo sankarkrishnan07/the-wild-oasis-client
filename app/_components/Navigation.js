c491509897d45620478bcb490a9b1731597004d8
sankarkrishnan07 <krishna07off@gmail.com>
2024-12-28 23:04:15 +0530

    Update: Implemented authentication

--- app/_components/Navigation.js
+++ app/_components/Navigation.js
@@ -1,6 +1,8 @@
 import Link from "next/link";
+import { auth } from "../_lib/auth";
 
-function Navigation() {
+async function Navigation() {
+  const session = await auth()
   return (
     <nav className="z-10 text-xl">
       <ul className="flex gap-16 items-center">
@@ -14,7 +16,16 @@ function Navigation() {
           <Link href="/about" className="hover:text-accent-400 transition-colors">About</Link>
         </li>
         <li>
-          <Link href="/account" className="hover:text-accent-400 transition-colors">Guest Area</Link>
+          <Link href="/account" className="hover:text-accent-400 transition-colors flex items-center gap-2">
+          {session?.user ?  <>
+            <img
+                className="h-8 rounded-full"
+                src={session.user.image}
+                alt={session.user.name}
+                referrerPolicy="no-referrer"
+              />
+              <span>Guest Area</span> </>: "Guest Area"}
+          </Link>
         </li>
       </ul>
     </nav>
